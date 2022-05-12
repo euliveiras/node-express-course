a45a1de07e96f3bdd6f2126e88f6400f7a563811
matheus oliveira <matheus.skm@hotmail.com>
2022-05-12 13:18:45 -0300

    refactor: took try/catch blocks from repository

--- task-manager/src/repositories/TasksRepository.ts
+++ task-manager/src/repositories/TasksRepository.ts
@@ -10,7 +10,7 @@ type UpdateTaskDTO = {
   id: string;
   taskName: string;
   isCompleted?: boolean;
-}
+};
 
 export default class TasksRepository {
   userRepository: Repository<Task>;
@@ -18,51 +18,34 @@ export default class TasksRepository {
     this.userRepository = typeorm.getRepository(Task);
   }
   async createTask({ name: taskName }: CreateTaskDTO) {
-    try {
       const taskCreated = this.userRepository.create({
         taskName,
         isCompleted: false,
       });
       await this.userRepository.save(taskCreated);
       return taskCreated;
-    } catch (err) {
-      console.error(err);
-    }
   }
 
-  async getAllTasks(): Promise<Task[] | undefined>{
-      try{
-          const tasks = await this.userRepository.find();
-          return tasks;
-      }catch(err){
-          console.error(err);
-      };
-    }
+  async getAllTasks(): Promise<Task[] | undefined> {
+      const tasks = await this.userRepository.find();
+      return tasks;
+  }
 
-    async updateTask({ id, taskName, isCompleted }: UpdateTaskDTO){
-      try{
-        const updatedTask = await this.userRepository.update(id, { taskName, isCompleted});
-        return updatedTask;
-      } catch(err){
-        console.error(err);
-      }
-    }
+  async updateTask({ id, taskName, isCompleted }: UpdateTaskDTO) {
+      const updatedTask = await this.userRepository.update(id, {
+        taskName,
+        isCompleted,
+      });
+      return updatedTask;
+  }
 
-    async deleteTask({ id }: {id: string}){
-      try{
-        const deletedTask = await this.userRepository.delete(id);
-        return { message: "task deleted!"};
-      } catch(err){
-        console.error(err);
-      }
-    }
+  async deleteTask({ id }: { id: string }) {
+      const deletedTask = await this.userRepository.delete(id);
+      return { message: "task deleted!" };
+  };
 
-    async getTask({ id }: { id: string }){
-      try{
-        const findedTask = await this.userRepository.findBy({ id });
-        return findedTask;
-      }catch(err){
-        throw new Error(err.message)
-      }
-    };
-};
+  async getTask({ id }: { id: string }) {
+      const findedTask = await this.userRepository.findBy({ id });
+      return findedTask;
+  }
+}
