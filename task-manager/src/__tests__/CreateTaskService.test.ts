4e05d6b663de042b1ce6506e70b6cfa8b33baacb
matheus oliveira <matheus.skm@hotmail.com>
2022-05-18 20:39:37 -0300

    test: it should throw error when the task name is a empty string

--- task-manager/src/__tests__/CreateTaskService.test.ts
+++ task-manager/src/__tests__/CreateTaskService.test.ts
@@ -1,5 +1,6 @@
 import { tasks } from "../mocks/tasks";
 import { createTaskService } from "../services/CreateTaskService";
+import { appError } from "../utils/AppError";
 
 jest.mock("../repositories/TasksRepository");
 jest.mock("../config/typeorm");
@@ -8,5 +9,8 @@ describe("CreateTaskService", () => {
   it("should create a new task with the given task name", async () => {
     const taskName = "Sleep";
     await expect(createTaskService.execute(taskName)).resolves.toHaveProperty("taskName", taskName);
+  });
+  it("should throw error when the task name is a empty string", async () => {
+    await expect(createTaskService.execute("")).rejects.toThrow(appError("Name must not be empty!", 400));
   });
 });
