360ba163bdbc1af9f3cb722e029d6c2d21171a26
matheus oliveira <matheus.skm@hotmail.com>
2022-05-18 13:36:43 -0300

    refactor: refac orm to not initialize DB class in testes

--- task-manager/src/__tests__/GetAllTasksService.test.ts
+++ task-manager/src/__tests__/GetAllTasksService.test.ts
@@ -1,10 +1,8 @@
 import {jest} from '@jest/globals'
-import Task from '../entities/Task';
-import MockedTaskRepository from "../repositories/MockedTaskRepository";
-import TasksRepository from "../repositories/TasksRepository";
-import GetAllTasksService, {getAllTasksService} from "../services/GetAllTasksService";
+import {getAllTasksService} from "../services/GetAllTasksService";
 
 jest.mock("../repositories/TasksRepository");
+jest.mock("../config/typeorm");
 
 describe("GetTaskService", () => {
     it("should return all tasks", async () => {
--- task-manager/src/config/typeorm.ts
+++ task-manager/src/config/typeorm.ts
@@ -1,22 +1,26 @@
 import { DataSource } from "typeorm";
 
-const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "adm",
-  password: "test",
-  database: "postgresDB",
-  entities: ["./src/entities/*.ts"],
-  migrations: ["./src/migrations/*.ts"],
-});
+let AppDataSource = {} as DataSource;
 
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization", err);
+if (process.env.ENVIRONMENT !== "TEST") {
+  AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "adm",
+    password: "test",
+    database: "postgresDB",
+    entities: ["./src/entities/*.ts"],
+    migrations: ["./src/migrations/*.ts"],
   });
+
+  AppDataSource.initialize()
+    .then(() => {
+      console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+      console.error("Error during Data Source initialization", err);
+    });
+}
 
 export default AppDataSource;
