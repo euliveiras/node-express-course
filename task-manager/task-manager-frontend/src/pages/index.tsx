82b287c2e3def122af3ca009565e5a774a22e9b1
matheus oliveira <matheus.skm@hotmail.com>
2022-05-24 20:50:59 -0300

    feat: added tasks state in home component

--- task-manager/task-manager-frontend/src/pages/index.tsx
+++ task-manager/task-manager-frontend/src/pages/index.tsx
@@ -2,8 +2,22 @@ import type { NextPage } from "next";
 import { Box, Flex, VStack } from "@chakra-ui/react";
 import { Form } from "../components/Form";
 import { Task } from "../components/Task";
+import { useState } from "react";
+
+type ITasks = {
+  id: string;
+  task: string;
+  isCompleted: boolean;
+}[];
 
 const Home: NextPage = () => {
+  const [tasks, setTasks] = useState<ITasks>(() => {
+    return [
+      { id: "1", task: "Jantar", isCompleted: true },
+      { id: "2", task: "Almoçar", isCompleted: false },
+    ];
+  });
+
   return (
     <Flex justifyContent={"center"}>
       <VStack spacing={16}>
@@ -17,9 +31,8 @@ const Home: NextPage = () => {
           <Form />
         </Box>
 
-        <VStack w={640}  spacing={4} >
-          <Task />
-          <Task />
+        <VStack w={640} spacing={4}>
+          {tasks.map((task) => <Task key={task.id}/>)}
         </VStack>
       </VStack>
     </Flex>
