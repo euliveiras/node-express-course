ad3ccdb18f22e00b47ac0132f659c6971f455a41
matheus oliveira <matheus.skm@hotmail.com>
2022-05-26 14:06:46 -0300

    feat: added form to edit tasks details

--- task-manager/task-manager-frontend/src/pages/task/[slug].tsx
+++ task-manager/task-manager-frontend/src/pages/task/[slug].tsx
@@ -1,3 +1,20 @@
+import {
+  Box,
+  Button,
+  Center,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { Field, Formik } from "formik";
 import { GetStaticPaths, GetStaticProps } from "next";
 
 type TaskProps = {
@@ -5,8 +22,94 @@ type TaskProps = {
 };
 
 export default function Task({ id }: TaskProps) {
-  console.log(id);
-  return <h1>{id}</h1>;
+  if (id) {
+    return (
+      <Flex justifyContent={"center"}>
+        <Box
+          w={640}
+          mt={32}
+          bgColor={"#FFFFFF"}
+          boxShadow={"lg"}
+          borderRadius={"lg"}
+        >
+          <Box w={"100%"}>
+            <Formik
+              initialValues={{ taskId: id, taskName: "", isCompleted: true }}
+              onSubmit={(value) => {
+                console.log(value);
+              }}
+            >
+              {({ handleSubmit, errors, touched }) => (
+                <VStack
+                  spacing={8}
+                  py={8}
+                  mx={20}
+                  as="form"
+                  onSubmit={handleSubmit}
+                >
+                  <Text fontFamily={"heading"} fontWeight={"700"} fontSize={24}>
+                    Edit task
+                  </Text>
+                  <InputGroup w="100%">
+                    <Center justifyContent={"space-between"} w="100%">
+                      <FormLabel htmlFor="taskId" m={0}>
+                        Task id
+                      </FormLabel>
+                      <Field
+                        isReadOnly
+                        w="auto"
+                        name="taskId"
+                        id="taskId"
+                        type="text"
+                        as={Input}
+                      />
+                    </Center>
+                  </InputGroup>
+                  <InputGroup w="100%">
+                    <Center justifyContent={"space-between"} w="100%">
+                      <FormLabel htmlFor="taskName" m={0}>
+                        Name
+                      </FormLabel>
+                      <Field
+                        w="auto"
+                        name="taskName"
+                        id="taskName"
+                        type="text"
+                        as={Input}
+                      />
+                    </Center>
+                  </InputGroup>
+                  <InputGroup w="100%">
+                    <Center justifyContent={"space-between"} w="100%">
+                      <FormLabel htmlFor="isCompleted" m={0}>
+                        Is completed?
+                      </FormLabel>
+                      <Field
+                        w="auto"
+                        name="isCompleted"
+                        id="isCompleted"
+                        type="checkbox"
+                        as={Checkbox}
+                      />
+                    </Center>
+                  </InputGroup>
+                  <Button
+                    variant={"solid"}
+                    borderRadius={4}
+                    colorScheme="purple"
+                    type="submit"
+                  >
+                    edit task
+                  </Button>
+                </VStack>
+              )}
+            </Formik>
+          </Box>
+        </Box>
+      </Flex>
+    );
+  }
+  return <Text>Loading</Text>;
 }
 
 export const getStaticPaths: GetStaticPaths = () => {
